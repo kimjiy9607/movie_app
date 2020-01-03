2fb546983a3ca10281fd18b54bc4a6fcf6996b5e
kimjiy <31414202+kimjiy@users.noreply.github.com>
2020-01-03 12:54:59 +0900

    Updates

---
+++ precache-manifest.f6d327569d5d8187edf76f799e38433b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64986578eb3ac94fa50a82ef1d248d18",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "cb0bff7e5932f40ce6fa",
+    "url": "/movie_app/static/css/main.380dd1c3.chunk.css"
+  },
+  {
+    "revision": "34efeaef8a43fcf0a5f8",
+    "url": "/movie_app/static/js/2.31ce3539.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie_app/static/js/2.31ce3539.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cb0bff7e5932f40ce6fa",
+    "url": "/movie_app/static/js/main.8df4c296.chunk.js"
+  },
+  {
+    "revision": "3dd3f4ef526798ea7005",
+    "url": "/movie_app/static/js/runtime-main.ec52c1d1.js"
+  }
+]);
